2b8bc077f17b7abca9160f5ab187fcff177060a8
Joe Fearnley <joe.fearnley@gmail.com>
2024-04-23 10:38:49 -0400

    Cleaning things up

--- pine-react/src/main.jsx
+++ pine-react/src/main.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css'; // Remove if nothing is visible
-import '@ionic/react/css/typography.css';
-import { setupIonicReact } from '@ionic/react';
-
-setupIonicReact();
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
