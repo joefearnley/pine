a2ee3426a3d87fbd584aa064fcb47d3ee77de2fd
Joe Fearnley <joe.fearnley@gmail.com>
2024-04-28 21:52:50 -0400

    Adding vanilla js scrollable library to work with react

--- pine-react/src/App.jsx
+++ pine-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { App, 
   NavRight, 
   Panel, 
@@ -8,10 +8,13 @@ import { App,
   BlockTitle, 
   Button, 
   List,
+  ListGroup,
   ListItem,
   Icon
 } from 'framework7-react';
-import { ReactSortable } from "react-sortablejs";
+import Sortable, { MultiDrag, Swap } from 'sortablejs';
+
+Sortable.mount(new MultiDrag(), new Swap());
 
 export default () => {
   const [players, setPlayers] = useState([
@@ -21,21 +24,54 @@ export default () => {
     { id: 4, name: "white" },
   ]);
 
-  return (
-    /* Main Framework7 App component where we pass Framework7 params */
-    <App theme="auto" name="My App">
+  useEffect(() => {
+    const playersOnFieldEl = document.querySelector('#players-on-field ul');
+    Sortable.create(playersOnFieldEl, {
+      group: 'shared',
+      multiDrag: true,
+      selectedClass: 'selected',
+      animation: 150,
+      onAdd: function (evt) {
+          const playerId = parseInt(evt.item.dataset.id);
+          console.log(`moving player go field: ${playerId}`);
+          // Roster.movePlayerToField(playerId);
+      },
+    });
 
-      {/*  Main view */}
+    const playersOnBenchtEl = document.querySelector('#players-on-bench ul');
+    Sortable.create(playersOnBenchtEl, {
+      group: 'shared',
+      multiDrag: true,
+      selectedClass: 'selected',
+      animation: 150,
+      onAdd: function (evt) {
+          const playerId = parseInt(evt.item.dataset.id);
+          console.log(`moving player go bench: ${playerId}`);
+          // Roster.movePlayerToBench(playerId);
+      },
+    });
+  }, []);
+
+  return (
+    <App theme="auto" name="Pine">
       <View main>
         <Page>
           <Navbar title="Awesome App"></Navbar>
           <BlockTitle>Playing</BlockTitle>
-          <List dividersIos simpleList strong outline>
-            <ReactSortable list={players} setList={setPlayers}>
-              {players.map((item) => (
-                <ListItem key={item.id} title={item.name} />
-              ))}
-            </ReactSortable>
+          <List id="players-on-field" dividersIos simpleList strong outline>
+            <ListGroup>
+                {players.map((item) => (
+                  <ListItem key={item.id} title={item.name} />
+                ))}
+            </ListGroup>
+          </List>
+          <BlockTitle>Bench</BlockTitle>
+          <List id="players-on-bench" dividersIos simpleList strong outline>
+            <ListGroup>
+                {players.map((item) => (
+                  <ListItem key={item.id} title={item.name} />
+                ))}
+            </ListGroup>
           </List>
         </Page>
       </View>
