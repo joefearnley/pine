a9257a2043d835c17c280640fa5e785431df76bd
Joe Fearnley <joe.fearnley@gmail.com>
2024-05-22 21:11:50 -0400

    Setting up roster page with new api

--- pine-react/src/pages/Roster.jsx
+++ pine-react/src/pages/Roster.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { createClient } from "@supabase/supabase-js";
+
 import {
     f7,
     Page,
@@ -14,18 +16,28 @@ import {
     Preloader,
 } from 'framework7-react';
 
+const supabase = createClient('https://dsmpjsdsczwgkafubpga.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzbXBqc2RzY3p3Z2thZnVicGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzMzY5NjIsImV4cCI6MjAzMTkxMjk2Mn0.j_Eo_4mV2M0sFm9hFiH2MvmYnEh8nO37DK6T2HdN1tw');
+
 const RosterPage = () => {
-    let players = useStore('players');
+    const [players, setPlayers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        f7.store.dispatch('getPlayers');
+        setLoading(true);
+        getPlayers();
     }, []);
+
+    async function getPlayers() {
+        const { data } = await supabase.from("players").select();
+        setPlayers(data);
+        setLoading(false);
+    }
 
     return (
         <Page name="roster">
             <Navbar title="Roster" />
             <BlockTitle>Players</BlockTitle>
-            {players && (
+            {players.length && (
                 <List dividersIos simpleList strong outline>
                     <ListGroup>
                         {players.map((item) => (
@@ -34,6 +46,10 @@ const RosterPage = () => {
                     </ListGroup>
                 </List>
             )}
+
+            <Block className="text-align-center">
+            {loading && <Preloader />}
+            </Block>
 
             <Toolbar bottom tabbar>
                 <Link 
