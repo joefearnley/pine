468e62b219fa8b2bb31d13e6183c8984c47e6583
Joe Fearnley <joe.fearnley@gmail.com>
2024-05-22 21:53:58 -0400

    Adding api to home page

--- pine-react/src/pages/Home.jsx
+++ pine-react/src/pages/Home.jsx
@@ -15,60 +15,73 @@ import Sortable, { MultiDrag, Swap } from 'sortablejs';
 Sortable.mount(new MultiDrag(), new Swap());
 
 const HomePage = () => {
-    const [players, setPlayers] = useState([
-        { id: 1, name: "shrek" },
-        { id: 2, name: "fiona" },
-        { id: 3, name: "black" },
-        { id: 4, name: "white" },
-    ]);
+    const [playersPlaying, setPlayersPlaying] = useState([]);
+    const [playersNotPlaying, setPlayersNotPlaying] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const playersOnFieldEl = document.querySelector('#players-on-field ul');
-        Sortable.create(playersOnFieldEl, {
-            group: 'shared',
-            multiDrag: true,
-            selectedClass: 'selected',
-            animation: 150,
-            onAdd: function (evt) {
-                const playerId = parseInt(evt.item.dataset.id);
-                console.log(`moving player go field: ${playerId}`);
-                // Roster.movePlayerToField(playerId);
-            },
-        });
+        // const playersOnFieldEl = document.querySelector('#players-players ul');
+        // Sortable.create(playersOnFieldEl, {
+        //     group: 'shared',
+        //     multiDrag: true,
+        //     selectedClass: 'selected',
+        //     animation: 150,
+        //     onAdd: function (evt) {
+        //         const playerId = parseInt(evt.item.dataset.id);
+        //         console.log(`moving player go field: ${playerId}`);
+        //         // Roster.movePlayerToField(playerId);
+        //     },
+        // });
     
-        const playersOnBenchtEl = document.querySelector('#players-on-bench ul');
-        Sortable.create(playersOnBenchtEl, {
-          group: 'shared',
-          multiDrag: true,
-          selectedClass: 'selected',
-          animation: 150,
-          onAdd: function (evt) {
-              const playerId = parseInt(evt.item.dataset.id);
-              console.log(`moving player go bench: ${playerId}`);
-              // Roster.movePlayerToBench(playerId);
-          },
-        });
+        // const playersOnBenchtEl = document.querySelector('#players-not-playing ul');
+        // Sortable.create(playersOnBenchtEl, {
+        //   group: 'shared',
+        //   multiDrag: true,
+        //   selectedClass: 'selected',
+        //   animation: 150,
+        //   onAdd: function (evt) {
+        //       const playerId = parseInt(evt.item.dataset.id);
+        //       console.log(`moving player go bench: ${playerId}`);
+        //       // Roster.movePlayerToBench(playerId);
+        //   },
+        // });
+
+        setPlayers();
       }, []);
+
+      async function setPlayers() {
+        const { players } = await supabase.from("players").select();
+        console.log(players);
+
+        setPlayersPlaying(players);
+        setLoading(false);
+    }
 
     return (
         <Page name="home">
             <Navbar title="Home" />
             <BlockTitle>Playing</BlockTitle>
-            <List id="players-on-field" dividersIos simpleList strong outline>
-                <ListGroup>
-                    {players.map((item) => (
-                        <ListItem key={item.id} title={item.name} />
-                    ))}
-                </ListGroup>
-            </List>
+            {playersNotPlaying.length && (
+                <List id="players-playing" dividersIos simpleList strong outline>
+                    <ListGroup>
+                        {playersPlaying.map((item) => (
+                            <ListItem key={item.id} title={item.name} />
+                        ))}
+                    </ListGroup>
+                </List>
+            )}
+
             <BlockTitle>Bench</BlockTitle>
-            <List id="players-on-bench" dividersIos simpleList strong outline>
-                <ListGroup>
-                    {players.map((item) => (
-                        <ListItem key={item.id} title={item.name} />
-                    ))}
-                </ListGroup>
-            </List>
+            {playersNotPlaying.length && (
+                <List id="players-not-playing" dividersIos simpleList strong outline>
+                    <ListGroup>
+                        {playersNotPlaying.map((item) => (
+                            <ListItem key={item.id} title={item.name} />
+                        ))}
+                    </ListGroup>
+                </List>
+             )}
+
             <Toolbar bottom tabbar>
                 <Link 
                     href="/"
