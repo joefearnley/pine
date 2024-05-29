fdbd06eb98a56532602a06e49fc323d1ef74aaea
Joe Fearnley <joe.fearnley@gmail.com>
2024-05-29 09:09:41 -0400

    another user test

--- api/tests/Feature/UserTest.php
+++ api/tests/Feature/UserTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+
 it('must be authenticated to access user data', function () {
     $response = $this->getJson(route('users.index'));
 
@@ -9,11 +12,16 @@ it('must be authenticated to access user data', function () {
         ]);
 });
 
-it('must be authenticated to access user data', function () {
+it('user can see own data', function () {
+    $user = User::factory()->create();
+
+    Sanctum::actingAs($user, ['*']);
+
     $response = $this->getJson(route('users.index'));
 
-    $response->assertStatus(401)
-        ->assertJson([
-            'message' => 'Unauthenticated.'
+    $response->assertStatus(200)
+        ->assertJsonFragment([
+            'name' => $user->name,
+            'email' => $user->email,
         ]);
 });
