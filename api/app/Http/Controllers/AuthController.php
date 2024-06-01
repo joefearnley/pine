0e81e12e27db5b29ae2ccd2fd73a887370dfcf3b
Joe Fearnley <joe.fearnley@gmail.com>
2024-05-31 22:22:40 -0400

    setting up auth controller

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Authenticate a user and return an access token.
+     */
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TeamController;
+use App\Http\Controllers\AuthController;
 
 Route::middleware(['auth:sanctum'])
     ->prefix('v1')
     ->group(function () {
         Route::resource('users', UserController::class);
         Route::resource('teams', TeamController::class);
+        Route::post('/account/login', [AuthController, 'authenticate']);
 });
