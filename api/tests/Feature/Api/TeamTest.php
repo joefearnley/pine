b0bd70fdcb0f6feeb53e498a065cd804876ef666
Joe Fearnley <joe.fearnley@gmail.com>
2024-06-08 11:46:18 -0400

    Adding more team tests

--- api/tests/Feature/Api/TeamTest.php
+++ api/tests/Feature/Api/TeamTest.php
@@ -24,11 +24,55 @@ it('can access team data', function () {
 
     Sanctum::actingAs($user, ['*']);
 
-    $response = $this->getJson(route('teams.index'));
-
-    $response->assertStatus(200)
+    $this->getJson(route('teams.index'))
+        ->assertStatus(200)
         ->assertJsonFragment([
             'name' => $team->name,
             'user_id' => $user->id,
+        ]);
+});
+
+it('cannot access team data from a different account', function () {
+    $user = User::factory()->create();
+    $otherUser = User::factory()->create();
+
+    $team = Team::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+    $otherTeam = Team::factory()->create([
+        'user_id' => $otherUser->id,
+    ]);
+
+    Sanctum::actingAs($user, ['*']);
+
+    $this->getJson(route('teams.index'))
+        ->assertStatus(200)
+        ->assertJsonFragment([
+            'name' => $team->name,
+            'user_id' => $user->id,
+        ])
+        ->assertJsonMissing([
+            'name' => $otherTeam->name,
+            'user_id' => $otherUser->id,
+        ]);
+});
+
+it('cannot access team data when no teams exist', function () {
+    $user = User::factory()->create();
+    $otherUser = User::factory()->create();
+
+    $otherTeam = Team::factory()->create([
+        'user_id' => $otherUser->id,
+    ]);
+
+    Sanctum::actingAs($user, ['*']);
+
+    $response = $this->getJson(route('teams.index'))
+        ->assertStatus(200)
+        ->assertJsonCount(0)
+        ->assertJsonMissing([
+            'name' => $otherTeam->name,
+            'user_id' => $otherUser->id,
         ]);
 });
