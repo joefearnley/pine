034883eeeae10a60188347d67a5c7b9bc64705e8
Joe Fearnley <joe.fearnley@gmail.com>
2024-06-14 22:24:39 -0400

    Getting update player initial tests working

--- api/tests/Feature/Api/Players/UpdatePlayerTest.php
+++ api/tests/Feature/Api/Players/UpdatePlayerTest.php
@@ -1,7 +1,50 @@
 <?php
 
-it('has updateplayer page', function () {
-    $response = $this->get('/updateplayer');
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use App\Models\Team;
+use App\Models\Player;
+use Laravel\Sanctum\Sanctum;
 
-    $response->assertStatus(200);
+uses(RefreshDatabase::class);
+
+it('requires a user to be authenitcated when updating a player', function () {
+    $user = User::factory()->create();
+
+    $team = Team::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+    $player = Player::factory()->create([
+        'team_id' => $team->id,
+    ]);
+
+    $postData = [];
+
+    $response = $this->putJson(route('players.update', $player), $postData)
+        ->assertStatus(401)
+        ->assertJson(['message' => 'Unauthenticated.']);
+});
+
+it('requires a name, and number for creating a player', function () {
+    $user = User::factory()->create();
+
+    $team = Team::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+    $player = Player::factory()->create([
+        'team_id' => $team->id,
+    ]);
+
+    $postData = [];
+
+    Sanctum::actingAs($user, ['*']);
+
+    $this->putJson(route('players.update', $player), $postData)
+        ->assertStatus(422)
+        ->assertJsonFragment(
+            ['The name field is required.'],
+            ['The number field is required.'],
+        );
 });
