63b4fdc590cb64cc9b9eae25206c3d2338284e37
Joe Fearnley <joe.fearnley@gmail.com>
2024-07-10 21:34:26 -0400

    Attempting local storage

--- pine-react/src/store.js
+++ pine-react/src/store.js
@@ -1,82 +1,24 @@
-// First import createStore function from Framework7 core
 import { createStore } from 'framework7/lite';
-import { LowSync } from 'lowdb';
-import { LocalStorage } from 'lowdb/browser';
-
-const defaultData = {
-    teams: [
-        {
-            "name": 'Team 1'
-        }
-    ],
-    players: [
-        {
-            "id": 0,
-            "name": "Asher",
-            "number": 13,
-            "isPlaying": true
-        },
-        {
-            "id": 1,
-            "name": "Bode",
-            "number": 12,
-            "isPlaying": true
-        },
-        {
-            "id": 2,
-            "name": "Deezhon",
-            "number": 45,
-            "isPlaying": true
-        },
-        {
-            "id": 3,
-            "name": "Ewan",
-            "number": 22,
-            "isPlaying": true
-        },
-        {
-            "id": 4,
-            "name": "Harrison",
-            "isPlaying": true
-        },
-        {
-            "id": 5,
-            "name": "JahReekis",
-            "isPlaying": true
-        },
-        {
-            "id": 6,
-            "name": "Jayce",
-            "isPlaying": false
-        },
-        {
-            "id": 7,
-            "name": "Jase",
-            "isPlaying": true
-        }
-    ],
-};
-
-const pineDB = new LowSync(new LocalStorage('pine_db'), defaultData);
-pineDB.write()
 
 const store = createStore({
     state: {
         loading: false,
+        team: {},
         players: [],
     },
     actions: {
         getPlayers({ state }) {
             state.loading = true;
 
-            pineDB.read();
+            state.team = localStorage.getItem('pinedb-team') || [];
+            state.players = localStorage.getItem('pinedb-players') || [];
 
-            state.players = pineDB.data.players;
             state.loading = false;
         },
     },
     getters: {
         loading: ({ state }) => state.loading,
+        team: ({ state }) => state.team,
         players: ({ state }) => state.players,
         playersPlaying: ({ state }) => state.players.filter(player => player.isPlaying),
         playersOnBench: ({ state }) => state.players.filter(player => !player.isPlaying),
